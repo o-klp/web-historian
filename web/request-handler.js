fe5015508372d3efc3f622245a113c786358a247
Hack Reactor Students <klp2012@gmail.com>
2014-08-13 15:39:45 -0700

    passes first spec - serving up some sweet html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,20 @@
 var path = require('path');
+var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers');
+var httpRequest = require('http-request');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+exports.handleRequest = function (request, response) {
+  var statusCode = 200;
+  console.log('got a request', request.url);
+
+  var pathName = request.url;
+
+  fs.readFile(archive.paths[pathName], function(error, data){
+    if(error) throw error;
+    response.writeHead(statusCode, helpers.headers);
+    response.end(data);
+  });
+
 };
